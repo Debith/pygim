37d9fa2aba943acf7f5cdecf66f7c71e1c5d7a76
Debith <Debith@users.noreply.github.com>
2022-07-06 13:45:17 +0100

    First working CLI (#2)
    Co-authored-by: Teppo Perä <teppo.pera@shell.com>

---
+++ pygim-common/pygim/__main__.py
@@ -0,0 +1,16 @@
+"""
+Python Gimmicks Command-Line Interface.
+"""
+
+import click
+
+@click.group()
+def cli():
+    """\b
+     ___       ___ _
+    | _ \_  _ / __(_)_ __
+    |  _/ || | (_ | | '  \ \b
+    |_|  \_, |\___|_|_|_|_|
+        |__/Python Gimmicks
+
+    """
