3356a09f8bbee7dee3e6ceab3fe600e7c75647a1
Teppo Perä <debith@iki.fi>
2022-07-09 14:52:07 +0100

    Adding tests, documentation and performance

--- pygim-common/pygim/utils/iterable.pyx
+++ pygim-common/pygim/utils/iterable.pyx
@@ -3,42 +3,59 @@
 This module contains miscellenious utility functions that can't be fit anywhere else.
 """
 
+import pygim.kernel.typing_ext as t
 from typing import Iterable, Callable, Any
 
-__all__ = ('split', 'flatten', 'is_container')
+__all__ = ['split', 'is_container', 'flatten']
 
-def split(iterable: Iterable[Any], condition: Callable):
+
+cpdef split(it: t.Iterable[Any], condition: t.Callable):
     """
     Split a iterable object into two, based on given condition.
-    """
-    left = []
-    right = []
 
-    for it in iterable:
-        if condition(it):
-            left.append(it)
+    Args:
+        it (Iterable[Any]): Iterator to be split in two.
+        condition (Callable): Function decide the way of splitting.
+
+    Returns:
+        result (Tuple): Two lists where left side has values matching the
+                        condition and right side has the rest.
+    """
+
+    cdef list left = []
+    cdef list right = []
+
+    for i in range(len(it)):
+        if condition(it[i]):
+            left.append(it[i])
         else:
-            right.append(it)
+            right.append(it[i])
 
     return left, right
 
 
-def is_container(obj):
+cpdef is_container(obj: t.Any):
     """ Checks whether the object is container or not.
+
     Container is considered an object, which includes other objects,
     thus string is not qualified, even it implments iterator protocol.
+
     >>> is_container("text")
     False
+
     >>> is_container(tuple())
     True
     """
-    if isinstance(obj, (str, bytes)):
+    if isinstance(obj, (str, bytes, type)):
         return False
 
-    return hasattr(obj, '__iter__')
+    if hasattr(obj, '__iter__'):
+        return True
+
+    return isinstance(obj, memoryview)
 
 
-def flatten(items):
+cpdef flatten(items: t.Iterable[t.Any]):
     """ Flatten the nested arrays into single one.
 
     Example about list of lists.
@@ -53,9 +70,13 @@ def flatten(items):
     >>> list(flatten(["one", "two", ["three", "four"]]))
     ['one', 'two', 'three', 'four']
     """
+    cdef list new_items = []
+
     for subitem in items:
         if is_container(subitem):
             for item in flatten(subitem):
-                yield item
+                new_items.append(item)
         else:
-            yield subitem
+            new_items.append(subitem)
+
+    return new_items
