6f8229421dcd01c6a1dafc242c0afbd0140b81ad
Teppo Perä <debith@iki.fi>
2022-08-26 18:27:12 +0100

    Fixing one more test

--- pygim-common/tests/conftest.py
+++ pygim-common/tests/conftest.py
@@ -6,7 +6,11 @@ import pathlib
 @pytest.fixture
 def temp_dir():
     tdir = tempfile.TemporaryDirectory()
+    __tdir = pathlib.Path(tdir.name)
 
-    yield pathlib.Path(tdir.name)
+    yield __tdir
 
+    __tdir.mkdir(exist_ok=True, parents=True)  # If tests removed, recreate.
     tdir.cleanup()
+
+    assert not __tdir.exists()
