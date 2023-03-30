9a419a8aa382df919d328e52496d760a17801eff
Teppo <debith-dev@@outlook.com>
2023-03-30 21:32:46 +0100

    playing

--- docs/examples/descriptors/type_descriptor.py
+++ docs/examples/descriptors/type_descriptor.py
@@ -2,10 +2,30 @@ from dataclasses import dataclass, field
 from pygim import TypeDescriptor
 
 
+@dataclass
 class Name:
+    _name: str
+
+
+@dataclass
+class Person:
+    name: Name = field(default_factory=Name)
+
+
+person = Person("example")
+assert isinstance(person.name, str)
+
+
+@dataclass
+class Name:
+    _name: str
     field = TypeDescriptor()
 
 
 @dataclass
 class Person:
     name: Name = Name.field()
+
+
+person = Person("new example")
+assert isinstance(person.name, Name)
