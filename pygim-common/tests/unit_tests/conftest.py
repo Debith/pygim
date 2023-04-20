56b690361f9c770171d7c0405bf35e0a11dae340
Debith <debith-dev@outlook.com>
2023-04-20 20:09:43 +0100

    adding magic

--- pygim-common/tests/unit_tests/conftest.py
+++ pygim-common/tests/unit_tests/conftest.py
@@ -13,3 +13,19 @@ def temp_dir():
     assert __tdir.exists(), "DO NOT DELETE TEMP DIR!"
 
     tdir.cleanup()
+
+import importlib.util
+import sys
+
+ROOT = pathlib.Path(__file__).parents[3]
+
+
+@pytest.fixture()
+def importer():
+    def importer(relative_path):
+        path = ROOT / relative_path
+        spec = importlib.util.spec_from_file_location(str(relative_path).replace("/", "."), path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+    return importer
---
+++ pygim-common/tests/unit_tests/magic/test_class_magic.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+def test_adding_trait_function_via_class(importer):
+    # Import module from magic directly.
+    mod = importer("pygim-common/pygim/kernel/magic/traits.py")
+
+    relocator = mod.Relocator()
+
+    class First:
+        def first(self): pass
+
+    class Second:
+        def second(self): pass
+
+    relocator(First, Second, ['second'])
+
+    assert hasattr(First, "first")
+    assert hasattr(First, "second")
+
+    assert hasattr(Second, "second")
+    assert not hasattr(Second, "first")
+
+
+def test_added_functions_are_truly_part_of_the_new_class(importer):
+    class First:
+        def __init__(self):
+            self.public = 42
+            self._protected = 43
+            self.__private = 44
+
+        def first(self):
+            return self.public + self._protected + self.__private
+
+    class Second:
+        def __init__(self):
+            self.public = 1000
+            self._protected = 100
+            self.__private = 10
+
+        def second(self):
+            return self.public - self._protected - self.__private
+
+    mod = importer("pygim-common/pygim/kernel/magic/traits.py")
+
+    relocator = mod.Relocator()
+    relocator(First, Second, ['second'])
+
+    first_inst = First()
+    assert first_inst.first() == 42 + 43 + 44
+    assert first_inst.second() == 42 - 43 - 44
+
+
+
+def test_combiner_merges_multiple_classes_elegantly(importer):
+
+    class First:
+        def first(self): pass
+
+    class Second:
+        def second(self): pass
+
+    class Third:
+        def third_a(self): pass
+        def third_b(self): pass
+
+    mod = importer("pygim-common/pygim/kernel/magic/traits.py")
+    CombinedClass = mod.combine(First, Second, Third)
+
+    assert hasattr(CombinedClass, "first")
+    assert hasattr(CombinedClass, "second")
+    assert hasattr(CombinedClass, "third_a")
+    assert hasattr(CombinedClass, "third_b")
+
+    assert not hasattr(First, "second")
+    assert not hasattr(Second, "first")
+    assert not hasattr(Third, "first")
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
